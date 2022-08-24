923bf8eb2db479c5389e8b8200f9a410b79a45f2
obatarhe <degraftekyirefi@gmail.com>
2022-08-24 02:54:09 +0000

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for one in range(97, 123):
-    if not(chr(one) == 'q' and chr(one) == 'e'):
+    if not(chr(one) == 'q' or chr(one) == 'e'):
         print("{}".format(chr(one)), end="")
